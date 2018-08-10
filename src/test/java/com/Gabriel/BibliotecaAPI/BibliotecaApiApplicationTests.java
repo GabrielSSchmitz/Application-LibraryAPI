0c652d3418e5d9300a40cf71592386ad65632b6a
Gabriel Schmitz <gschmitz.geomais@gmail.com>
2018-08-10 10:36:20 -0300

    Ajuste JUnitTest

--- src/test/java/com/Gabriel/BibliotecaAPI/BibliotecaApiApplicationTests.java
+++ src/test/java/com/Gabriel/BibliotecaAPI/BibliotecaApiApplicationTests.java
@@ -2,16 +2,16 @@ package com.Gabriel.BibliotecaAPI;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized.Parameters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
 public class BibliotecaApiApplicationTests {
 
 	@Test
-	public void contextLoads() {
-		System.out.println("Teste OK  ");
+	public void thisTestIsOK() {
+		System.out.println("Teste OK");
 	}
 
 }
