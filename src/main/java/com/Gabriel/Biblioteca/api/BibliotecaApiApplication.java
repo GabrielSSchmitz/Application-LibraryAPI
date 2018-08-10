c82e902648364d31ed59ee6938e9f3e05f23d1a3
Gabriel <gschmitz.geomais@gmail.com>
2018-08-10 11:44:18 -0300

    Ajustes git clone 2

--- src/main/java/com/Gabriel/Biblioteca/api/BibliotecaApiApplication.java
+++ src/main/java/com/Gabriel/Biblioteca/api/BibliotecaApiApplication.java
@@ -10,6 +10,5 @@ public class BibliotecaApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApiApplication.class, args);
 		
-		System.out.println("sdf");
 	}
 }
