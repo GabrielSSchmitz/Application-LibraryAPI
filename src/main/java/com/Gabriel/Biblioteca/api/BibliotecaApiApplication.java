48c4e8e402a115720cb44a41f63bf3011cbc738a
Gabriel Schmitz <gschmitz.geomais@gmail.com>
2018-08-10 10:07:35 -0300

    Ajustes testes

--- src/main/java/com/Gabriel/Biblioteca/api/BibliotecaApiApplication.java
+++ src/main/java/com/Gabriel/Biblioteca/api/BibliotecaApiApplication.java
@@ -9,5 +9,7 @@ public class BibliotecaApiApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApiApplication.class, args);
+		
+		System.out.println("sdf");
 	}
 }
--- src/test/java/com/Gabriel/BibliotecaAPI/BibliotecaApiApplicationTests.java
+++ src/test/java/com/Gabriel/BibliotecaAPI/BibliotecaApiApplicationTests.java
@@ -9,5 +9,9 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class BibliotecaApiApplicationTests {
 
+	@Test
+	public void contextLoads() {
+		System.out.println("Teste OK");
+	}
 
 }
