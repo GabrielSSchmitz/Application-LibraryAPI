bb03878db134a9d03d5f9688d0bce0ae736932ee
Gabriel <gschmitz.geomais@gmail.com>
2018-08-10 11:42:15 -0300

    Ajustes git Clone

--- src/test/java/com/Gabriel/BibliotecaAPI/BibliotecaApiApplicationTests.java
+++ src/test/java/com/Gabriel/BibliotecaAPI/BibliotecaApiApplicationTests.java
@@ -2,8 +2,6 @@ package com.Gabriel.BibliotecaAPI;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized.Parameters;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
